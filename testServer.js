57cab5273ad9da46a687044ba83ef49fa6e63876
Jin <ll1124@hanmail.net>
2018-08-22 01:52:28 +0900

    error edit

--- testServer.js
+++ testServer.js
@@ -6,18 +6,21 @@ var ejs = require('ejs');
 var mongoose = require('mongoose');
 
 var testServer = express();
+require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 testServer.use(bodyParser.urlencoded({extended:true})); // bodyParser을 이용하기 위한 코드
 testServer.set('views', __dirname+'/views'); // project의 directory name이 views인 폴더내부의 파일을 보겠다는 의미
 testServer.set('view engine','ejs'); // .ejs파일을 보는 view engine을 사용한다는 코드
 
-var uri = process.env.MONGOLAB_URI;
+var uri = process.env.MONGO_URI;
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open',function(){
   console.log("Connected to mongod server");
 });
-mongoose.connect(uri,{ useNewUrlParser: true });
+mongoose.connect(uri,{ useNewUrlParser: true }).catch(e=> {
+  console.error(e.message);
+});
 
 testServer.use('/TSMcss', express.static(__dirname+'/public/stylesheets/jobmain.css')); //  /TSMcss의 css파일을 jobmain.css로 설정하는 코드
 testServer.use('/TSAcss', express.static(__dirname+'/public/stylesheets/jobadd.css')); //  /TSAcss의 css파일을 jobadd.css로 설정하는 코드
